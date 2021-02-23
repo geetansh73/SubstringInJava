0b654538e3420811b2775300ca32be0f8506b6ac
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-02-23 14:43:48 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class  substring{
+	public static void main (String[] args) {
+	String s="hello";
+	System.out.println(s.substring(0,2));
+	System.out.println(s.substring(1));
+String str=	s.substring(3);
+System.out.println(s);
+System.out.println(str);
+System.out.println(s.substring(1,2));
+	
+	
+	
+	
+	}}
+	
